190ecf51e1246af2ed1f147d6c41fb9c8f65b421
andiahmads <andi.fivesco@gmail.com>
2022-06-18 17:39:19 +0700

    add create account API & setup router

---
+++ api/server.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	db "github.com/andiahmads/simple-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+	return server
+}
+
+// start runs the HTPP server on  spesific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
